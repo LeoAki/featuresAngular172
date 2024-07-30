78684b0bb622bf9338e1757f309bf5427ec0278f
Leonardo Aquino <laquino@soaint.com>
2024-07-30 15:36:48 -0500

    Feat: Image-Service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalModelComponent } from './model-signal/signal-model/signal-model.component';
+import { QueryViewComponent } from './signal-query/query-view/query-view.component';
+import { SearchModule } from './signal-query/query-content/search/search.module';
+import { NgOptimizedImage, provideImgixLoader } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, SignalModelComponent, QueryViewComponent, SearchModule,
+    NgOptimizedImage
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    provideImgixLoader("https://maciejwojcik.imgix.net")
+  ]
 })
 export class AppComponent {
-  title = 'angular172';
+  title = 'Angular 17.2';
 }
